578543116b8437b2c0570912a6b092eb8d6f0d91
AndriiKryshtop <andrewstudio7@gmail.com>
2017-08-15 21:34:03 +0300

    Initial commit

---
+++ src/main/java/Group3/lab3/city_overview/CityOverviewApplication.java
@@ -0,0 +1,12 @@
+package Group3.lab3.city_overview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityOverviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CityOverviewApplication.class, args);
+	}
+}
---
+++ src/test/java/Group3/lab3/city_overview/CityOverviewApplicationTests.java
@@ -0,0 +1,16 @@
+package Group3.lab3.city_overview;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CityOverviewApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
